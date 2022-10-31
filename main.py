7fa03470a8bf395b0af7e67048b4464a7a0365c1
shigeyuu <shigeyuu05@gmail.com>
2022-10-31 21:24:29 +0900

    second commit

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import streamlit as st
-
+import datetime as dt
+import date_process.calc_date as dc
 
 st.title('お産予測アプリ')
 
+date_injection = st.date_input('注射日を選択してください。')
+
+if date_injection:
+
+    st.write('15日前の日付')
+    st.write(date_injection-dt.timedelta(days=15))
+    st.write('45日前の日付')
+    st.write(date_injection-dt.timedelta(days=45))
